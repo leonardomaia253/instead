97849d3bef0d1ad40899066097afabe6b282e5c4
leonardomaia253 <leonardomaia253@gmail.com>
2026-03-16 17:08:20 +0000

    seer

--- frontend/src/i18n/request.ts
+++ frontend/src/i18n/request.ts
@@ -1,6 +1,6 @@
 import { getRequestConfig } from 'next-intl/server';
 import { notFound } from 'next/navigation';
-import { locales } from './navigation';
+import { locales } from '../navigation';
 
 export default getRequestConfig(async ({ locale }) => {
   if (!locales.includes(locale as any)) notFound();
